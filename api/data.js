5b7b8d20264f40b8a933acdc5845522e73502f76
Creckeerz <61243933+creckeerz@users.noreply.github.com>
2025-06-28 06:22:28 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,10 +1,21 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbz698wTJ7_-BclMcEziWXNaTNUOcxRhTLCAPngcBVzOysRsI1351JgTEstWoxYaMcrz/exec');
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    console.error('Gagal ambil data:', err);
-    res.status(500).json({ error: 'Gagal ambil data dari Google Sheets' });
+    const response = await fetch(
+      'https://script.google.com/macros/s/AKfycbz698wTJ7_-BclMcEziWXNaTNUOcxRhTLCAPngcBVzOysRsI1351JgTEstWoxYaMcrz/exec',
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    const json = await response.json();
+
+    // Anti cache
+    res.setHeader('Cache-Control', 'no-store');
+    res.status(200).json(json);
+  } catch (error) {
+    console.error('Fetch Data Error:', error);
+    res.status(500).json({ error: 'Gagal ambil data dari Spreadsheet' });
   }
 }
+
