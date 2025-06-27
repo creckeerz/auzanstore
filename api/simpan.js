4178671672ffb4b41f0dca8b4f2f27681a39220b
Creckeerz <61243933+creckeerz@users.noreply.github.com>
2025-06-28 06:38:49 +0700

    Update simpan.js

--- api/simpan.js
+++ api/simpan.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      'https://script.google.com/macros/s/AKfycbz698wTJ7_-BclMcEziWXNaTNUOcxRhTLCAPngcBVzOysRsI1351JgTEstWoxYaMcrz/exec',
+      'https://script.google.com/macros/s/AKfycbwtJPPYdJ1gwkCbCo0DDNzTFzkw66ZsOde-4Z4l1_hPzpI87x6e9pace6-QAmdYlgBd/exec',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
