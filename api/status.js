dd6155651a591fa183468496b49953de09f6f22e
Creckeerz <61243933+creckeerz@users.noreply.github.com>
2025-06-28 07:07:03 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,7 +1,7 @@
 eexport default async function handler(req, res) {
   try {
     const response = await fetch(
-      'https://script.google.com/macros/s/AKfycbz698wTJ7_-BclMcEziWXNaTNUOcxRhTLCAPngcBVzOysRsI1351JgTEstWoxYaMcrz/exec?mode=status'
+      'https://script.google.com/macros/s/AKfycbwtJPPYdJ1gwkCbCo0DDNzTFzkw66ZsOde-4Z4l1_hPzpI87x6e9pace6-QAmdYlgBd/exec?mode=status'
     );
     const status = await response.text();
     res.status(200).send(status.trim());
