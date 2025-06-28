645c8541045af52c7250b68cdb882a908756e848
Creckeerz <61243933+creckeerz@users.noreply.github.com>
2025-06-28 13:52:23 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      'https://script.google.com/macros/s/AKfycbxlpjSKq8NiLax8eWctSh8SBKNTUgr1Kxw5I2XWFWkMw0hKH-1fsZieqxVxuGN8ZPE9/exec
-',
+      'https://script.google.com/macros/s/AKfycbyLq-XIXuF5TkSiROre1A7JEj5-hPjYWbVr4VvmUh9aDD3MnXU2LntTkGdpAPZkz1cx/exec',
       {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
