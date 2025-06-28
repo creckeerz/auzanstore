3e474cec55d22b6b18f5ce6f7a2c04ce3ccedfdc
Creckeerz <61243933+creckeerz@users.noreply.github.com>
2025-06-28 15:44:33 +0700

    Update maintenance.js

--- api/maintenance.js
+++ api/maintenance.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const status = req.body.status;
 
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwFnXGXb0CLX9oOxdJ7mWkPcy8DU5B1lPsjFdMDznG78vT_kaNiVtFCARP-ufHm3pGc/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwcH3MsaF5T2K8X5hsuPsx3ZJoP_FKOcmC8TO-whJuUfwa6PHFB3wPXsokRR6XV13Gm/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ status })
