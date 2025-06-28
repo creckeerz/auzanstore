ebc16a7552c1ae41fd8e2167618c86ced78f31e4
Creckeerz <61243933+creckeerz@users.noreply.github.com>
2025-06-28 08:19:55 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const baseUrl = 'https://script.google.com/macros/s/AKfycbwC_ArGcfNNIp1jlE-jdU7GOVfFz7EoWmqG255UJ2lNQ2l4SD3qwAOEXZjGfLKjI13H/exec';
+  const baseUrl = 'https://script.google.com/macros/s/AKfycbxuRfaWHE77oX3F8k1pjQr9OcymGT_S4Y1Sf8SSRXkXtwRrrdvQ6jdxsSP0jaNA6rFj/exec';
 
   try {
     if (req.method === 'GET') {
